fce05e48a4a8d7ec122600e7cdb51509a4a8ca69
MeenakshiSelvam <sssmeena207@gmail.com>
2023-08-22 12:24:11 +0530

    adding launch browsers

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,22 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class App {
+    public static void main(String[] args) {
+        EdgeDriver driver = new EdgeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.navigate().refresh();
+        driver.manage().window().fullscreen();
+        driver.manage().window().minimize();
+        driver.navigate().back();
+        driver.navigate().forward();
+
+
+
     }
+
+
 }
